069729daf2410fff8c99603fa92ea614693ad05a
Rubaeid-Sanjid <rubaeid.sanjid@gmail.com>
2024-04-27 03:55:10 +0600

    tailwind, daisyUI, route initial setup

---
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,11 @@
+
+
+const ErrorPage = () => {
+    return (
+        <div>
+            <h2>Oops, Dont worry its just a simple mistake.</h2>
+        </div>
+    );
+};
+
+export default ErrorPage;
