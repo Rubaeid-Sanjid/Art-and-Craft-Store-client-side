d3cb771c1fdb436f33aa77bbd0fbdda296be655f
Rubaeid-Sanjid <rubaeid.sanjid@gmail.com>
2024-05-01 04:18:37 +0600

    error page emoji added

--- src/Pages/ErrorPage/ErrorPage.jsx
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -1,10 +1,12 @@
 import { Link } from "react-router-dom";
+import emoji from "../../assets/sad-face-emoji.png"
 
 const ErrorPage = () => {
   return (
     <div className="hero min-h-screen bg-base-200">
       <div className="hero-content text-center">
-        <div>
+        <div className="flex flex-col items-center">
+            <img src={emoji} alt="emoji" className="w-40 mb-6" />
           <h1 className="text-5xl font-bold mb-2">404</h1>
           <h1 className="text-4xl font-bold mb-4">Page not found.</h1>
           <Link to="/">
