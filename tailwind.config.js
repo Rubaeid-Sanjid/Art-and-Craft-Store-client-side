4c68343043e922bd9e13979571c42b7283558cd7
Rubaeid-Sanjid <rubaeid.sanjid@gmail.com>
2025-06-18 12:11:39 +0600

    fixed daisy ui issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,15 @@
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: [
+//     "./index.html",
+//     "./src/**/*.{js,ts,jsx,tsx}",
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [require("daisyui")],
+// }
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -5,8 +17,21 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'base-100': '#ffffff', // Your custom color for base-100
+      },
+    },
+  },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "base-100": "#ffffff",  // Custom background color for bg-base-100
+          "base-200": "#f4f4f4",  // Optional: Custom background for bg-base-200
+        },
+      },
+    ],
   },
   plugins: [require("daisyui")],
 }
-
